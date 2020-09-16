cd90261244bd7d086597f70c4bfb629449d663fc
Christian Ehlers <70565547+ChristianEhlers@users.noreply.github.com>
2020-09-16 11:34:08 +0200

    Add files via upload

---
+++ jsDom1.js
@@ -0,0 +1,48 @@
+function createCookie(name, value, days) {
+    let expires;
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == " ") {
+            c = c.substring(1,c.length);
+        }
+        if (c.indexOf(nameEQ) == 0) {
+            return c.substring(nameEQ.length, c.length);
+        }
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name, "", -1);
+}
+
+const $ = function(foo){
+	return document.getElementById(foo);
+}
+const handleSubmit = function(){
+	let name = document.form11.name;
+	let bday = document.form11.bday;
+	let content = name + bday;
+	createCookie(content, 42, 0.00694444);
+	console.log(document.cookie);
+	prevent.Default();
+}
+const doSomething = function(){
+	document.addEventListener("submit", handleSubmit);
+}
+window.addEventListener("load", doSomething);
+
